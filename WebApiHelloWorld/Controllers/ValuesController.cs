f06e9980405702c192c13b7bd3bb8bb23e37310b
mikaelec <mikaelec@gmail.com>
2015-09-01 20:05:05 +0200

    Initial commit, should-be-working web api, not bloaty

---
+++ WebApiHelloWorld/Controllers/ValuesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using WebApiHelloWorld.Models;
+
+namespace WebApiHelloWorld.Controllers
+{
+    [RoutePrefix("api/values")]
+    public class ValuesController : ApiController
+    {
+        #region private fields
+        private int _nextId;
+        private IDictionary<int, ValueModel> _values;
+        #endregion
+
+        public ValuesController()
+        {
+            seed();
+        }
+
+        [Route("")]
+        [HttpGet]
+        public IHttpActionResult GetValues()
+        {
+            return Ok(_values);
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IHttpActionResult GetValue(int id)
+        {
+            var result = _values[id];
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [Route("")]
+        [HttpPost]
+        public IHttpActionResult PostValue([FromBody]ValueModel value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                _values.Add(_nextId++, value);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route("{id}")]
+        [HttpPut]
+        public IHttpActionResult PutValue(int id, [FromBody]ValueModel value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                _values[id] = value;
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IHttpActionResult DeleteValue(int id)
+        {
+            try
+            {
+                _values.Remove(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route("secret")]
+        [HttpGet]
+        [Authorize]
+        public IHttpActionResult SecretValue()
+        {
+            return Ok("Nah nah! You'll never get this!");
+        }
+
+        #region private methods
+        private void seed()
+        {
+            _nextId = 1;
+
+            _values = new Dictionary<int, ValueModel>();
+            _values.Add(_nextId++,
+                new ValueModel
+                {
+                    X = "abe",
+                    Y = "abe"
+                });
+            _values.Add(_nextId++,
+                new ValueModel
+                {
+                    X = "abe2",
+                    Y = "abe2"
+                });
+        }
+        #endregion
+    }
+}
---
+++ WebApiHelloWorld/Models/ValueModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiHelloWorld.Models
+{
+    public class ValueModel
+    {
+        [Display(Name = "X Value")]
+        [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [RegularExpression(".*", ErrorMessage = "How did you do that?")]
+        [DataType(DataType.Password, ErrorMessage = "{0} must be a password for a good reason.")]
+        public string X { get; set; }
+
+        [Display(Name = "Y Value")]
+        [Compare("X", ErrorMessage = "{1} must match {0} for whatever reason.")]
+        public string Y { get; set; }
+    }
+}
