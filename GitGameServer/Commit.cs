fd386c899e60dbca7f90adc0694bf96f7d4db4de
deaddog <mikkelslsl@gmail.com>
2015-09-10 16:37:11 +0200

    Implemented the Commit class as a simple container for managed commit objects.

--- GitGameServer/Commit.cs
+++ GitGameServer/Commit.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace GitGameServer
+﻿namespace GitGameServer
 {
     public class Commit
     {
+        private string message;
+        private string username;
+
+        private int added, removed;
+
+        public Commit(string message, string username, int added, int removed)
+        {
+            this.message = message;
+            this.username = username;
+
+            this.added = added;
+            this.removed = removed;
+        }
+
+        public string Message => message;
+        public string Username => username;
+
+        public int Added => added;
+        public int Removed => removed;
     }
 }
