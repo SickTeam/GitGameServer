5abf2614d9360f44a37c61fd1085004b1c881eea
deaddog <mikkelslsl@gmail.com>
2015-09-10 15:54:40 +0200

    Created a static helper (extension) class for building Stream IO methods.

---
+++ GitGameServer/StreamExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace GitGameServer
+{
+    public static class StreamExtension
+    {
+        public static void Write(this Stream stream, int value)
+        {
+            stream.Write(BitConverter.GetBytes(value), 0, 4);
+        }
+        public static void Write(this Stream stream, string value)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(value);
+            stream.Write(buffer.Length);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+
+        public static int ReadInt32(this Stream stream)
+        {
+            byte[] buffer = new byte[4];
+            stream.Read(buffer, 0, 4);
+            return BitConverter.ToInt32(buffer, 0);
+        }
+        public static string ReadString(this Stream stream)
+        {
+            int len = ReadInt32(stream);
+            byte[] buffer = new byte[len];
+            stream.Read(buffer, 0, buffer.Length);
+            return Encoding.UTF8.GetString(buffer);
+        }
+    }
+}
