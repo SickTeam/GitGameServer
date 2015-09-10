2c1c912dc25e21f96c90e675d24f35adc8bf1691
deaddog <mikkelslsl@gmail.com>
2015-09-10 16:08:43 +0200

    Created an enum describing the possible game settings.

---
+++ GitGameServer/GameSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitGameServer
+{
+    [Flags]
+    public enum GameSettings
+    {
+        // The numerical values below are intended as flags (1, 2, 4, 8, ...) and are used for persistency.
+        ExcludeMerges = 1,
+        LowerCase = 2
+    }
+}
