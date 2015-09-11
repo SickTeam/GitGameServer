c5fd02d02c9b08ea7c92a20660d20d1cecf74bc1
deaddog <mikkelslsl@gmail.com>
2015-09-11 07:24:11 +0200

    Created a class representing contributor settings.

---
+++ GitGameServer/Models/Contributor.cs
@@ -0,0 +1,14 @@
+﻿namespace GitGameServer.Models
+{
+    public class Contributor
+    {
+        public Contributor(string name)
+        {
+            this.Name = name;
+            this.Active = true;
+        }
+
+        public string Name { get; private set; }
+        public bool Active { get; set; }
+    }
+}
