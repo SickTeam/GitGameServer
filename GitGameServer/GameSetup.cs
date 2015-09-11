ee42a4b9cd1065c9b79339e423a7722f63116931
deaddog <mikkelslsl@gmail.com>
2015-09-11 07:25:33 +0200

    Added SetContributor/GetContributors to GameSetup.

--- GitGameServer/GameSetup.cs
+++ GitGameServer/GameSetup.cs
@@ -20,6 +20,7 @@ namespace GitGameServer
             return true;
         }
 
+        private List<Models.Contributor> contributors;
         private List<User> users;
         
         public GameSetup(string owner, string repo, IEnumerable<Octokit.GitHubCommit> commits)
@@ -30,10 +31,10 @@ namespace GitGameServer
             this.repository = repo;
 
             this.commits = commits.ToArray();
+            this.settings = GameSettings.None;
             this.count = commits.Count(filter);
 
-            this.settings = GameSettings.None;
-
+            this.contributors = new List<Models.Contributor>();
             this.users = new List<User>();
         }
 
@@ -59,6 +60,44 @@ namespace GitGameServer
             var user = User.Create(username);
             this.users.Add(user);
             return user;
+        }
+        public void SetContributor(string name, bool active)
+        {
+            for (int i = 0; i < contributors.Count; i++)
+                if (contributors[i].Name == name)
+                {
+                    contributors[i].Active = active;
+                    return;
+                }
+
+            foreach (var c in GetContributors())
+                if (c.Name == name)
+                {
+                    contributors.Add(new Models.Contributor(name) { Active = active });
+                    return;
+                }
+
+            return;
+        }
+
+        public Models.Contributor[] GetContributors()
+        {
+            List<Models.Contributor> list = new List<Models.Contributor>();
+            foreach (var c in commits)
+            {
+                var con = list.Find(x => x.Name == c.Commit.Committer.Name);
+                if (con == null)
+                    list.Add(new Models.Contributor(c.Commit.Committer.Name) { Active = filter(c) });
+                else if (!con.Active && filter(c))
+                    con.Active = true;
+            }
+            foreach(var c in list)
+            {
+                var t = contributors.FirstOrDefault(x => x.Name == c.Name);
+                c.Active = t.Active;
+            }
+
+            return list.ToArray();
         }
     }
 }
