92107071f981c4307991b270dfe6c0f819113a9f
deaddog <mikkelslsl@gmail.com>
2015-09-15 07:33:45 +0200

    Implemented the method that loads a Game object from a file.

--- GitGameServer/Game.cs
+++ GitGameServer/Game.cs
@@ -14,6 +14,14 @@ namespace GitGameServer
 
         private readonly string owner, repository;
 
+        private User[] users;
+        private string[] contributors;
+
+        private long tableStart;
+        private int rowSize;
+        private int rowCount;
+        private int tableIndex;
+
         public static Game FromSetup(GameSetup setup, string filepath)
         {
             using (FileStream fs = new FileStream(filepath, FileMode.CreateNew))
@@ -48,7 +56,47 @@ namespace GitGameServer
 
         public static Game FromFile(string filepath)
         {
-            throw new NotImplementedException();
+            using (FileStream fs = new FileStream(filepath, FileMode.Open))
+            {
+                string owner = fs.ReadString();
+                string repo = fs.ReadString();
+                string token = fs.ReadString();
+                byte set = (byte)fs.ReadInt32();
+
+                Game game = new Game(filepath, token, owner, repo);
+
+                game.users = new User[fs.ReadInt32()];
+                for (int i = 0; i < game.users.Length; i++)
+                    game.users[i] = User.FromStream(fs);
+
+                game.contributors = new string[fs.ReadInt32()];
+                for (int i = 0; i < game.contributors.Length; i++)
+                    game.contributors[i] = fs.ReadString();
+
+                game.rowCount = fs.ReadInt32();
+
+                game.tableStart = fs.Position;
+                game.rowSize = 40 + game.users.Length;
+
+                for (game.tableIndex = 0; game.tableIndex < game.rowCount; game.tableIndex++)
+                {
+                    fs.Seek(40, SeekOrigin.Current);
+                    bool stop = false;
+
+                    for (int i = 0; i < game.users.Length; i++)
+                        if (fs.ReadByte() == 0)
+                        {
+                            stop = true;
+                            fs.Seek(-(40 + i + 1), SeekOrigin.Current);
+                            break;
+                        }
+
+                    if (stop)
+                        break;
+                }
+
+                return game;
+            }
         }
 
         private Game(string path, string token, string owner, string repo)
