db640789c5013832c249b364a252319b5ff79878
deaddog <mikkelslsl@gmail.com>
2015-09-19 20:23:59 +0200

    Created a type for managing the details of a single api message.

---
+++ GitGameServer/Message.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace GitGameServer
+{
+    public class Message
+    {
+        private DateTime timestamp;
+        private string name;
+        private string url;
+        private JObject resource;
+
+        public Message(DateTime timestamp, string name, string url, JObject resource = null)
+        {
+            this.timestamp = timestamp;
+            this.name = name;
+            this.url = url;
+            this.resource = resource;
+        }
+
+        public DateTime Timestamp => timestamp;
+        public string Name => name;
+        public string URL => url;
+        
+        public JObject ToJObject()
+        {
+            var obj = new JObject()
+            {
+                {"name", name },
+                {"url", url }
+            };
+
+            if (resource != null)
+                obj.Add("resource", resource);
+
+            return obj;
+        }
+    }
+}
