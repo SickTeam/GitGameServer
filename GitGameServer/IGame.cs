4ccf5d06f98f855981c2a44a538888c90b8d0bf8
deaddog <mikkelslsl@gmail.com>
2015-09-19 20:39:17 +0200

    Added a method to IGame that retrieves all messages associated with a game.

--- GitGameServer/IGame.cs
+++ GitGameServer/IGame.cs
@@ -1,7 +1,11 @@
-﻿namespace GitGameServer
+﻿using System.Collections.Generic;
+
+namespace GitGameServer
 {
     public interface IGame
     {
         string State { get; }
+
+        IEnumerable<Message> GetMessages();
     }
 }
