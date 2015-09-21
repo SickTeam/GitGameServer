9ddfa45e8db57d2ad190bbab01399ce0dda64b4c
deaddog <mikkelslsl@gmail.com>
2015-09-21 14:45:30 +0200

    Created a GetMessagesSince extension method, implemented using Linq.

--- GitGameServer/IGame.cs
+++ GitGameServer/IGame.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GitGameServer
 {
@@ -7,5 +9,13 @@ namespace GitGameServer
         string State { get; }
 
         IEnumerable<Message> GetMessages();
+    }
+
+    public static class IGameExtension
+    {
+        public static IEnumerable<Message> GetMessagesSince(this IGame game, DateTime since)
+        {
+            return game.GetMessages().SkipWhile(x => x.Timestamp < since);
+        }
     }
 }
