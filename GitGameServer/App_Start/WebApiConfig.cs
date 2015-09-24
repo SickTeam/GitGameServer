b2fa1e6f2081481f8a17f731d38598a42363df73
mikaelec <mikaelec@gmail.com>
2015-09-24 19:18:42 +0200

    Added CORS support, closes #9

--- GitGameServer/App_Start/WebApiConfig.cs
+++ GitGameServer/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace GitGameServer
 {
@@ -7,6 +8,8 @@ namespace GitGameServer
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
         }
     }
 }
